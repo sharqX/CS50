ef8c4e164cbde0acc969f417039f9ebd71f30741
sharqX <shariquerahman24@outlook.com>
2024-01-31 20:47:25 +0530

    added class for exceptions

--- Python/4.Exceptions/class/number.py
+++ Python/4.Exceptions/class/number.py
@@ -1,6 +1,30 @@
-try:
-    x = int(input("What's x? "))
-except ValueError:
-    print("x is not an integer")
-else:
- print(f"x is {x}")
+def main():
+    x = get_int()
+    print(f"s is {x}")
+
+
+def get_int():
+    while True:
+        try:
+            x = int(input("What's x? ")) #return int(input("What's x? "))
+        except ValueError:
+            print("x is not an integer") #pass
+        else:
+            return x    #we can return the input in the same line itself shown above
+
+main()
+
+
+# def main():
+#     x = get_int("What's x? ")
+#     print(f"s is {x}")
+
+
+# def get_int(promt):
+#     while True:
+#         try:
+#             return int(input(promt))
+#         except ValueError:
+#             pass
+#         
+# main()
