f5ec2f3e72ddca40f8169fc4ad29738582df3d6a
sharqX <shariquerahman24@outlook.com>
2024-02-14 23:03:23 +0530

    Added Unit Tests

---
+++ Python/6.Unit Tests/class/test_calculator2.py
@@ -0,0 +1,8 @@
+from calculator import square
+
+def test_sqaure():
+    assert square(2) == 4
+    assert square(3) == 9
+    assert square(-2) == 4
+    assert square(-3) == 9
+    assert square(0) == 0
