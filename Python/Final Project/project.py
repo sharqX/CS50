b0a4763acf98ddf16e64bf9768be9c6835248ff4
sharqX <shariquerahman24@outlook.com>
2024-04-17 19:33:41 +0530

    Final Code

--- Python/Final Project/project.py
+++ Python/Final Project/project.py
@@ -45,7 +45,7 @@ class Pomodoro:
         time_prompts = [
             ("Enter the time in hours: "),
             ("Enter the time in minutes: "),
-            ("Enter the time in seconds: ")
+            ("Enter the time in seconds: "),
         ]
         error_prompt = "Input must be a number. Please try again."
         while True:
@@ -110,6 +110,28 @@ class Pomodoro:
             for line in data:
                 file.write(line)
         Pomodoro.menu(self)
+        return h, m, s, h1, m1, s1
+
+    def test_settings(self, h, m, s, h1, m1, s1):
+        h = Pomodoro.settings
+        m = Pomodoro.settings
+        s = Pomodoro.settings
+        h1 = Pomodoro.settings
+        m1 = Pomodoro.settings
+        s1 = Pomodoro.settings
+        timer_inputs = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+        if not h or h not in timer_inputs:
+            raise ValueError
+        if not m or m not in timer_inputs:
+            raise ValueError
+        if not s or s not in timer_inputs:
+            raise ValueError
+        if not h1 or h1 not in timer_inputs:
+            raise ValueError
+        if not m1 or m1 not in timer_inputs:
+            raise ValueError
+        if not s1 or s1 not in timer_inputs:
+            raise ValueError
 
     def menu(self):
         table = [["1", "Start Timer"], ["2", "Change Timmer Settings"], ["3", "Exit"]]
@@ -129,6 +151,19 @@ class Pomodoro:
                     print("Not a correct option")
             except ValueError:
                 print("Not a correct option")
+            return pressed
+
+    def test_menu(self, p):
+        p = Pomodoro.menu
+        if p == 1:
+            print("Starting your timer")
+            Pomodoro.timer(self)
+        elif p == 2:
+            Pomodoro.settings(self)
+        elif p == 3:
+            sys.exit("See you again!")
+        else:
+            raise (ValueError)
 
     def conti(self):
         table = [["1", "Continue?"], ["2", "Change Timmer Settings"], ["3", "Exit"]]
@@ -147,6 +182,17 @@ class Pomodoro:
                     print("Not a correct option")
             except ValueError:
                 print("Not a correct option")
+
+    def test_conti(self, p):
+        p = Pomodoro.conti
+        if p == 1:
+            Pomodoro.timer(self)
+        elif p == 2:
+            Pomodoro.settings(self)
+        elif p == 3:
+            sys.exit("See you again!")
+        else:
+            raise ValueError
 
 
 def title():
